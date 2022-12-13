b8b09380d5d8cda002a901abe5c6f97c102d4aa1
BitTheCat <12282238+BitTheCat@users.noreply.github.com>
2022-12-13 23:52:25 +0100

    added sub component for table style v0.2.3

---
+++ src/components/TTr.vue
@@ -0,0 +1,14 @@
+<template>
+    <tr class="tv-tr">
+        <slot></slot>
+    </tr>
+</template>
+
+<script setup>
+import {defineComponent} from 'vue';
+
+defineComponent({
+    name: 'TTr',
+})
+
+</script>
