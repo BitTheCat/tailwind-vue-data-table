fadca6c64d1416627c011152b98d64aff59cc91c
BitTheCat <12282238+BitTheCat@users.noreply.github.com>
2023-01-19 20:13:05 +0100

    added first vitest test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
-   content: [
-    './index.html',
-    './dev/**/*.{html,js,vue,ts}',
-    './src/**/*.{html,js,vue,ts}',
-   ],
- }
+    content: [
+        './index.html',
+        './dev/**/*.{html,js,vue,ts}',
+        './src/**/*.{html,js,vue,ts}',
+    ],
+}
